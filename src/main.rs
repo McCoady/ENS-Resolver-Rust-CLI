32f635d6b30191aa9b876275a56dd12b1e7cc4db
McCoady <78250146+McCoady@users.noreply.github.com>
2022-09-15 23:16:17 +0900

    Remove infura

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::fs;
 async fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
+    let rpc_url = &args[2];
 
     let mut count = 0;
 
@@ -14,7 +15,7 @@ async fn main() {
 
     let addresses = fs::read_to_string(file_path).unwrap();
     for address in addresses.split_ascii_whitespace() {
-        let res = get_address(address);
+        let res = get_address(address, &rpc_url);
         block_on(res);
         count += 1;
     }
@@ -22,10 +23,8 @@ async fn main() {
     eprintln!("{} addresses", count);
 }
 
-async fn get_address(name: &str) {
-    let provider =
-        Provider::<Http>::try_from("https://mainnet.infura.io/v3/341158d7871b4b44a5471eba5bbc6e70")
-            .expect("Problem connecting to provider");
+async fn get_address(name: &str, rpc: &String) {
+    let provider = Provider::<Http>::try_from(rpc).expect("Problem connecting to provider");
     let address_result = provider.resolve_name(name).await;
 
     let _address = match address_result {
